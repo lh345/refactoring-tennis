1fb4f08a7b450790d6f31a5e359babb29999dfa3
Luo Huan <huan.luo@thoughtworks.com>
2020-03-07 17:05:39 +0800

    extract player class and long method

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -4,60 +4,53 @@ import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private Player player1;
+    private Player player2;
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        player1 = new Player(player1Name);
+        player2 = new Player(player2Name);
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName == player1.getName()) {
+            player1.win();
+        } else {
+            player2.win();
+        }
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore = 0;
-        if (m_score1 == m_score2) {
-            return getScoreWhenEquals();
+        int player1Score = player1.getScore();
+        int player2Score = player2.getScore();
+
+        if (isDeuce(player1Score, player2Score)) {
+            return getScoreWhenDeuce(player1Score);
+        }
+        if (isAdvantageAndWin(player1Score, player2Score)) {
+            return getScoreWhenAdvantageAndWin(player1Score, player2Score);
         }
 
-        if (m_score1 >= 4 || m_score2 >= 4) {
-            return getScoreWhenBothGreaterThanFour();
-        }
-        for (int i = 1; i < 3; i++) {
-            if (i == 1) {
-                tempScore = m_score1;
-            } else {
-                score += "-";
-                tempScore = m_score2;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
-        }
-        return score;
+        return getScoreWhenNormal(player1Score, player2Score);
     }
 
-    private String getScoreWhenBothGreaterThanFour() {
-        int minusResult = m_score1 - m_score2;
+    private boolean isDeuce(int score1, int score2) {
+        return score1 == score2;
+    }
+
+    private boolean isAdvantageAndWin(int score1, int score2) {
+        return score1 >= 4 || score2 >= 4;
+    }
+
+    private String getScoreWhenNormal(int score1, int score2) {
+        String[] scoreStringMap = {"Love", "Fifteen", "Thirty", "Forty"};
+
+        return scoreStringMap[score1] + "-" + scoreStringMap[score2];
+    }
+
+    private String getScoreWhenAdvantageAndWin(int score1, int score2) {
+        int minusResult = score1 - score2;
+
         if (minusResult == 1) {
             return "Advantage player1";
         } else if (minusResult == -1) {
@@ -69,8 +62,8 @@ public class TennisGame1 implements TennisGame {
         }
     }
 
-    private String getScoreWhenEquals() {
-        switch (m_score1) {
+    private String getScoreWhenDeuce(int score) {
+        switch (score) {
             case 0:
                 return "Love-All";
             case 1:
